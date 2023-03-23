336a85c0e7659a701d0f9db413b965fecbdd36c4
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-03-23 16:41:48 +0100

    :/

--- android/src/main/kotlin/com/lastgimbus/the/lastbluetooth/TheLastBluetoothPlugin.kt
+++ android/src/main/kotlin/com/lastgimbus/the/lastbluetooth/TheLastBluetoothPlugin.kt
@@ -17,6 +17,7 @@ import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import kotlinx.coroutines.*
 import java.io.IOException
 import java.util.*
