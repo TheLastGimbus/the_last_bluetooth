edab18e1c0d47ef5a4ffd4a4062b002a4ac9845f
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-12-09 21:44:38 +0100

    :(((
    help dart team!!

--- lib/the_last_bluetooth.dart
+++ lib/the_last_bluetooth.dart
@@ -1,7 +1,25 @@
 import 'dart:async';
 
+import 'package:jni/jni.dart';
+import 'package:the_last_bluetooth/src/android_bluetooth.g.dart';
+
 int sum(int a, int b) => a + b;
 
-Future<int> sumAsync(int a, int b) async {
-  return a + b;
+Future<int> sumAsync(int a, int b) async => a + b;
+
+// can't do!! help dart team :((
+class MyBroadcastReceiver extends BroadcastReceiver {
+  @override
+  void onReceive(Context context, Intent intent) {
+    super.onReceive(context, intent);
+    print(intent);
+  }
+}
+
+class TheLastBluetooth {
+  TheLastBluetooth() {
+    final ctx = Context.fromRef(Jni.getCachedApplicationContext());
+    final receiver = MyBroadcastReceiver();
+    ctx.registerReceiver(receiver, IntentFilter());
+  }
 }
