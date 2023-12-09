a6b39213e414581005006302952f5f749925866b
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-12-09 21:09:42 +0100

    new ground for jni <33333

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:the_last_bluetooth/the_last_bluetooth.dart';
+import 'dart:async';
+
+import 'package:the_last_bluetooth/the_last_bluetooth.dart' as the_last_bluetooth;
 
 void main() {
   runApp(const MyApp());
@@ -15,107 +15,57 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final bt = TheLastBluetooth.instance;
-  static const sppUuid = "00001101-0000-1000-8000-00805f9b34fb";
-
-  // Fallen into my own trap of "only first listen gets initial devices"
-  List<BluetoothDevice> devices = [];
-  BluetoothConnection? conn;
+  late int sumResult;
+  late Future<int> sumAsyncResult;
 
   @override
   void initState() {
     super.initState();
-    bt.pairedDevicesStream.listen((event) {
-      setState(() {
-        devices = event;
-      });
-    });
+    sumResult = the_last_bluetooth.sum(1, 2);
+    sumAsyncResult = the_last_bluetooth.sumAsync(3, 4);
   }
 
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 25);
+    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Native Packages'),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              StreamBuilder(
-                stream: bt.adapterInfoStream,
-                builder: (context, snapshot) {
-                  return snapshot.hasData
-                      ? Text(snapshot.data.toString())
-                      : const Text("No data");
-                },
-              ),
-              const Text("paired devs (NEW 💯 - STREAM):"),
-              // StreamBuilder<List<BluetoothDevice>>(
-              //   stream: bt.pairedDevicesStream,
-              //   initialData: [],
-              //   builder: (context, snapshot) {
-              //     if (snapshot.hasError) {
-              //       return Text("error: ${snapshot.error}");
-              //     }
-              //     if (snapshot.hasData != true) return const Text("wait...");
-              //     return Column(
-              //       children: snapshot.data!
-              //           .map((e) => Text(
-              //                 "${e.name} ; "
-              //                 "${e.alias ?? "null"} ; "
-              //                 "${e.address} ; "
-              //                 "${e.isConnected ? "✅" : "❌"}",
-              //               ))
-              //           .toList(),
-              //     );
-              //   },
-              // ),
-
-              // my own trap
-              ...devices
-                  .map((e) => Text(
-                        "${e.name} ; "
-                        "${e.alias ?? "null"} ; "
-                        "${e.address} ; "
-                        "${e.isConnected ? "✅" : "❌"}",
-                      ))
-                  .toList(),
-
-              // @Shit("Temporary shitty code to test sending"
-              TextButton(
-                onPressed: () async {
-                  try {
-                    conn = await bt.connectRfcomm(
-                        devices.firstWhere((e) => e.isConnected), sppUuid);
-                    setState(() {});
-                    await conn!.io.stream.listen(print).asFuture();
-                    conn = null;
-                    setState(() {});
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                child: Text("Connect"),
-              ),
-              if (conn != null)
-                TextButton(
-                  onPressed: () async {
-                    await conn!.io.sink.close();
-                    conn = null;
-                    setState(() {});
-                  },
-                  child: Text("close"),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                const Text(
+                  'This calls a native function through FFI that is shipped as source in the package. '
+                  'The native code is built as part of the Flutter Runner build.',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
                 ),
-              if (conn != null)
-                TextButton(
-                  onPressed: () {
-                    conn!.io.sink.add(Uint8List.fromList(
-                        [90, 0, 7, 0, 43, 4, 1, 2, 1, -1, -1, -20]));
-                  },
-                  child: Text("send"),
+                spacerSmall,
+                Text(
+                  'sum(1, 2) = $sumResult',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
                 ),
-            ],
+                spacerSmall,
+                FutureBuilder<int>(
+                  future: sumAsyncResult,
+                  builder: (BuildContext context, AsyncSnapshot<int> value) {
+                    final displayValue =
+                        (value.hasData) ? value.data : 'loading';
+                    return Text(
+                      'await sumAsync(3, 4) = $displayValue',
+                      style: textStyle,
+                      textAlign: TextAlign.center,
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
